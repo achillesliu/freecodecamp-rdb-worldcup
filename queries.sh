4a49b459cf9fef53be06e8185ab439be18edb926
bo <bo.ac.liu@gmail.com>
2023-03-17 23:24:17 +0100

    update: add foreign key queries for refer

--- queries.sh
+++ queries.sh
@@ -41,3 +41,21 @@ echo "$($PSQL "SELECT year, winner FROM games WHERE round='Final' ORDER BY year 
 
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
+
+# # Follwoing uses join with foreign keys.
+
+# echo -e "\nWinner of the 2018 tournament team name:"
+# echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE round='Final' AND year=2018")"
+# # 
+# echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
+# echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE round='Eighth-Final' AND year='2014' ORDER BY name")"
+# # 
+# echo -e "\nList of unique winning team names in the whole data set:"
+# echo "$($PSQL "SELECT DISTINCT(name) FROM teams INNER JOIN games ON teams.team_id = games.winner_id ORDER BY name")"
+
+# echo -e "\nYear and team name of all the champions:"
+# echo "$($PSQL "SELECT year, name FROM teams INNER JOIN games ON teams.team_id =games.winner_id WHERE round='Final' ORDER BY year")"
+# # 
+# echo -e "\nList of teams that start with 'Co':"
+# echo "$($PSQL "SELECT DISTINCT(name) FROM teams INNER JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE name LIKE 'Co%'")"
+# # 
